13c26ef73e21d269fc275230b7108fc7ae3fecb3
ikhachatryan93 <ikhachatryan93@gmail.com>
2018-05-27 06:13:25 -0600

    separating files

---
+++ src/bookingsync_to_mysql.py
@@ -0,0 +1,16 @@
+import logging_setup
+import logging
+import traceback
+from bookingsync import run_bookingsync
+logging_setup.configure_logging('stream')
+
+
+def main():
+    try:
+        run_bookingsync()
+    except:
+        logging.critical(traceback.format_exc())
+
+
+if __name__ == "__main__":
+    main()
